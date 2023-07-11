32fb30ef630fe1ca28b3a774f2e9618dd4317381
atuwar@myseneca.ca <atuwar@myseneca.ca>
2023-07-11 19:04:25 -0400

    feat : EDITED FILES

--- js/app.js
+++ js/app.js
@@ -1,21 +1,3 @@
-/**
- * WEB222 – Assignment 04
- *
- * I declare that this assignment is my own work in accordance with
- * Seneca Academic Policy. No part of this assignment has been
- * copied manually or electronically from any other source
- * (including web sites) or distributed to other students.
- *
- * Please update the following with your information:
- *
- *      Name:       <ARYAN_TUWAR>
- *      Student ID: <112137229>
- *      Date:       <NEED_TO_FILL>
- */
-
-// All of our data is available on the global `window` object.
-// Create local variables to work with it in this file.
-
 const { products, categories } = window;
 
 function main() {
